ac8c571c1d13501e4e7fd1af1e97e68169bfd961
IamMukhammed <abdullaevazizkhan@gmail.com>
2024-11-18 20:56:48 -0700

    RR: building express web server

---
+++ server.js
@@ -0,0 +1,46 @@
+console.log("Web serverni boshlash");
+const express = require("express");
+const app = express(); /// expresnijng app objectini yuboradi express serverini yuboradi
+const http = require("http");
+const fs = require("fs");
+const { start } = require("repl");
+app.use(express.static('public'));
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+ if (err) { 
+    console.log("EROR:",err);
+}
+ else {
+    user = JSON.parse(data);
+ }
+});
+
+//1 kirish kodlar
+app.use(express.static("public")); 
+app.use(express.json()); 
+app.use(express.urlencoded({extended: true })); 
+
+//3 views qismi
+app.set("views", "views");
+app.set("view engine", "ejs");
+
+// 4 routing qismi   
+
+app.post("/create-item", (req,res) => {
+   
+    });
+
+    app.get("/author", (req,res) => {
+      res.render("author", {user: user });
+    });
+    app.get('/', function (req, res) {
+        res.render("Harid")
+    }); 
+    
+    const server = http.createServer(app);
+    let PORT = 3000;
+    server.listen(PORT, function (){
+       console.log(`this server sucsesfully  running on port ${PORT}`) 
+    });
+    
